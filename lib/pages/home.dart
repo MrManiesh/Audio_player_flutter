07345bb2dc81064096a825e1dfca7ed04c97a36a
Machine Learning by Manish <z80030z@gmail.com>
2023-12-08 00:35:02 +0530

    Initial Commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Audio Player',    
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              
+        ),
+        ),
+        // backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            // color: Colors.amber,
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/three_lines.svg',
+            height: 20,
+            width: 20,),
+        ),
+        centerTitle: true,
+      )
+    );
+  }
+}
