9a0ba847a9b4707c04ee23122850b0668aa7081e
Machine Learning by Manish <z80030z@gmail.com>
2023-12-08 21:34:52 +0530

    added side drawer and a placeholder for song art

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,37 +2,95 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Audio Player',    
+          'Audio Player',
           style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              
-        ),
-        ),
-        // backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            // color: Colors.amber,
-            borderRadius: BorderRadius.circular(10)
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
           ),
-          child: SvgPicture.asset(
-            'assets/icons/three_lines.svg',
-            height: 20,
-            width: 20,),
+        ),
+        elevation: 0.0,
+        leading: Builder(
+          builder: (context) => GestureDetector(
+            onTap: () {
+              Scaffold.of(context).openDrawer();
+            },
+            child: Container(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/three_lines.svg',
+                height: 20,
+                width: 20,
+              ),
+            ),
+          ),
         ),
         centerTitle: true,
-      )
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 62, 17, 105),
+              ),
+              child: Text(
+                'Audio Player',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Song 1'),
+              onTap: () {
+                // Handle item 1 tap
+              },
+            ),
+            ListTile(
+              title: Text('Song 2'),
+              onTap: () {
+                // Handle item 2 tap
+              },
+            ),
+            // Add more ListTiles for additional items
+          ],
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(top: (40.0)),
+                child: Container(
+                  child: Image.asset(
+                    'assets/images/default_music_logo.png',
+                    // Adjust width and height as needed
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
